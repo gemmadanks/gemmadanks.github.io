99136674d713dd8c0515c9d6033d0f47e4175cc7
gemmadanks <gemmadanks@users.noreply.github.com>
2022-11-21 14:45:50 +0000

    jekyll build from Action 8d2258c7a65f6ee88162ee09ecc7b19052ca9840

---
+++ README.md
@@ -0,0 +1,10 @@
+# Open Research
+Welcome to my GitHub pages repo!
+
+This is where I store all the files for my [Open Research site](https://gemmadanks.github.io/).
+
+# License
+
+All source code is licensed under the [MIT license](https://github.com/gemmadanks/gemmadanks.github.io/blob/main/LICENSE).
+
+All other content on my site is licensed under the [Creative Commons Attribution 4.0 International license](https://creativecommons.org/licenses/by/4.0/).
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,73 @@
+var store = [{
+        "title": "Welcome to my open research site!",
+        "excerpt":"Welcome to my open research site! My name is Gemma Danks and I have a background in academic research (13 yrs) and tech consulting (5 yrs and counting). This is the place where I organise, document and share my open research projects, how I work on them and what I...","categories": ["about"],
+        "tags": ["about"],
+        "url": "/pages/gemmadanks/about/welcome-to-my-open-research-site/",
+        "teaser": null
+      },{
+        "title": "How to set up a GitHub pages website on a Mac",
+        "excerpt":"First things first. Getting started with open research involves choosing and setting up a way to share your work. I have previously created several websites (my personal website, my “astrobiology for kids” blog and my company website). For all of these I used WordPress. WordPress gives you a lot of...","categories": ["tutorials"],
+        "tags": ["setup","github","website","jekyll"],
+        "url": "/pages/gemmadanks/tutorials/how-to-set-up-github-pages-website/",
+        "teaser": null
+      },{
+        "title": "The benefits of process mapping and my research process",
+        "excerpt":"For several years I worked as a consultant in a large engineering firm, building advanced machine learning models for predictive maintenance of their equipment. During this time I was often asked to formally document (or map) the processes I was following when developing and deploying models. A process map is...","categories": ["planning"],
+        "tags": ["about","process","planning"],
+        "url": "/pages/gemmadanks/planning/my-research-process/",
+        "teaser": null
+      },{
+        "title": "How to set up a Bluehost subdomain for a GitHub pages site",
+        "excerpt":"I already have a personal website with a page about my previous research, so I decided to point my open research GitHub pages to a subdomain there rather than use the default GitHub pages url gemmadanks.github.io. This also means it will be easier to move my site if I ever...","categories": ["tutorials"],
+        "tags": ["setup","github","website","bluehost"],
+        "url": "/pages/gemmadanks/tutorials/how-to-set-up-a-bluehost-subdomain-for-github-pages/",
+        "teaser": null
+      },{
+        "title": "How to choose a research topic",
+        "excerpt":"For most of my academic career, my choice of research topic was driven by curiosity, some sort of “gut feeling”, and a desire to make a difference, discover something new and make a contribution towards human knowledge. In practice though, it was also heavily influenced by chance, availability of jobs...","categories": ["planning"],
+        "tags": ["planning","research","tools","decisions"],
+        "url": "/pages/gemmadanks/planning/choosing-research-topic/",
+        "teaser": null
+      },{
+        "title": "AI for decoding non-human languages",
+        "excerpt":"A.I. has become incredibly powerful for translating human languages. Could it be used to glean information from non-human languages? The impact of understanding non-human animal communication would be enormous. We would have the knowledge and the additional motivation to reduce their suffering on a global scale and improve conservation efforts....","categories": ["notes"],
+        "tags": ["planning","ai","decisions","language","non-human"],
+        "url": "/pages/gemmadanks/notes/ai-for-decoding-non-human-languages/",
+        "teaser": null
+      },{
+        "title": "Technosignatures",
+        "excerpt":"Technosignatures are signals that indicate the existence of technology (i.e. artifacts created from the practical application of scientific knowledge) – either intentional signals or by-products. Humanity generates several technosignatures – both on Earth and in orbit or on other planets in the solar system. Detecting technosignatures from non-human sources would...","categories": ["notes"],
+        "tags": ["planning","decisions","astrobiology","technosignatures","exoplanets"],
+        "url": "/pages/gemmadanks/notes/technosignatures/",
+        "teaser": null
+      },{
+        "title": "Recognising AI sentience",
+        "excerpt":"Right now the most advanced artificial intelligence is unaware of its existence and has no capacity to feel anything. It is based purely on a series of matrix multiplications with weights that are tuned to perform a task that we design. These tasks, however, are getting more and more complex...","categories": ["notes"],
+        "tags": ["planning","ai","decisions","sentience"],
+        "url": "/pages/gemmadanks/notes/recognising-ai-sentience/",
+        "teaser": null
+      },{
+        "title": "Exoplanet biosignatures",
+        "excerpt":"In the last few decades we have discovered that Earth-like planets are common around other stars and many of these planets are likely to be habitable, i.e. one or more environments on the planet can sustain life. A major focus of astrobiology research is on how to detect life existing...","categories": ["notes"],
+        "tags": ["planning","astrobiology","decisions","biosignatures","exoplanets"],
+        "url": "/pages/gemmadanks/notes/exoplanet-biosignatures/",
+        "teaser": null
+      },{
+        "title": "AI misuse: pathogenic DNA",
+        "excerpt":"Biotechnology has now advanced to the stage where it is not only possible to synthesise a DNA sequence of your choice in the lab, but to mail-order it from commercial DNA synthesis companies. This has huge benefits for research in the life sciences but it has also raised concerns about...","categories": ["notes"],
+        "tags": ["planning","ai","decisions","genomics","biosecurity"],
+        "url": "/pages/gemmadanks/notes/ai-misuse-pathogenic-dna/",
+        "teaser": null
+      },{
+        "title": "How to get an overview of a research topic in two days",
+        "excerpt":"I have spent about seven working days learning about five different topics that came out top in the decision matrix that resulted from my process for choosing a focus for my research going forward. You can read my notes about each topic here: AI for translating unseen languages Extraterrestrial technosignatures...","categories": ["tutorials"],
+        "tags": ["planning","research","decisions","process"],
+        "url": "/pages/gemmadanks/tutorials/how-to-get-an-overview-of-a-research-topic/",
+        "teaser": null
+      },{
+        "title": "Why I'm choosing technosignatures as my next research topic",
+        "excerpt":"The first step in any research cycle is to choose a topic. This time around, I used a decision matrix to rank a list of topics by their impact, neglectedness, feasibility and how well they fit my interests, skills and lifestyle. Since I was unsure of some of the scores...","categories": ["planning"],
+        "tags": ["planning","decisions","astrobiology","technosignatures","exoplanets"],
+        "url": "/pages/gemmadanks/planning/my-next-research-topic-technosignatures/",
+        "teaser": null
+      }]
